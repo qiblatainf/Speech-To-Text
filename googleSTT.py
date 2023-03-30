71dc87d0cb428f11435149b4cff774c3219da338
qiblatainf <qiblatain@live.com>
2023-03-30 18:05:50 +0500

    adding google API

---
+++ googleSTT.py
@@ -0,0 +1,8 @@
+
+# import csv
+import os
+# import boto3
+# import six
+from google.cloud import translate_v2 as translate
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/zaine/SadiqProjects/ml-kit-algo/translationtest-380502-cf110ae29be7.json"
