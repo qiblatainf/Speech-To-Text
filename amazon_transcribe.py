d5f59f6b037f52d980e39d41a143b9ca18cfcdf4
qiblatainf <qiblatain@live.com>
2023-04-11 15:02:19 +0500

    start transcribe job

--- amazon_transcribe.py
+++ amazon_transcribe.py
@@ -6,11 +6,33 @@ import csv
 with open("amazon-key.csv", 'r') as file:
   csvreader = csv.reader(file)
   for row in csvreader:
-    aws_access_key_idprint = row[0]
-    aws_secret_access_key = row[1]
+    access_key_id = row[0]
+    secret_access_key = row[1]
 
-# transcribe = boto3.client('transcribe',
-# aws_access_key_id = #insert your access key ID here,
-# aws_secret_access_key = # insert your secret access key here
-# region_name = # region: usually, I put "us-east-2"
-# ) 
+transcribe = boto3.client('transcribe',
+aws_access_key_id = access_key_id,
+aws_secret_access_key = secret_access_key,
+region_name = "us-east-2"
+) 
+
+#data is uploaded to the S3 Bucket
+BUCKET_NAME = "livestreamaudiobucket"
+FILE_NAME = "SM1_F1_A01.wav"
+JOB_NAME = "transcribe_job" # can be anything
+
+
+def start_transcribe_job(transcribe, job_name, bucket, file):
+    # this is the location of your audio file
+    file_uri = f'https://s3.amazonaws.com/{bucket}/{file}'
+    
+    # try launching the job and return any error messages
+    try:
+        transcribe.start_transcription_job(
+            TranscriptionJobName=job_name,
+            Media={'MediaFileUri': file_uri},
+            MediaFormat='mp3',
+            LanguageCode='en-US')
+        return True
+    except Exception as e:
+        return e
+    
