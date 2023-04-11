bbbeccf3219f7c93d079276e4b9f199d0539af30
qiblatainf <qiblatain@live.com>
2023-04-11 15:36:57 +0500

    amazon transcription added

--- amazon_transcribe.py
+++ amazon_transcribe.py
@@ -1,5 +1,3 @@
-import pandas as pd
-import time
 import boto3 #boto3 is the aws sdk for python
 
 import csv
@@ -12,27 +10,68 @@ with open("amazon-key.csv", 'r') as file:
 transcribe = boto3.client('transcribe',
 aws_access_key_id = access_key_id,
 aws_secret_access_key = secret_access_key,
-region_name = "us-east-2"
+region_name = "ap-south-1"
 ) 
 
 #data is uploaded to the S3 Bucket
 BUCKET_NAME = "livestreamaudiobucket"
 FILE_NAME = "SM1_F1_A01.wav"
-JOB_NAME = "transcribe_job" # can be anything
+JOB_NAME = "transcribe_job2" # can be anything
 
 
 def start_transcribe_job(transcribe, job_name, bucket, file):
     # this is the location of your audio file
     file_uri = f'https://s3.amazonaws.com/{bucket}/{file}'
-    
+    print("Starting Transcription Job")
     # try launching the job and return any error messages
     try:
         transcribe.start_transcription_job(
             TranscriptionJobName=job_name,
             Media={'MediaFileUri': file_uri},
-            MediaFormat='mp3',
+            MediaFormat='wav',
             LanguageCode='en-US')
         return True
     except Exception as e:
         return e
     
+    
+    
+def get_transcription_text(transcribe, job_name):
+
+    import urllib.request
+    import json
+    import time
+    
+    # let's obtain the job instance
+    job = transcribe.get_transcription_job(TranscriptionJobName=job_name)
+    
+    # and it's status
+    status = job['TranscriptionJob']['TranscriptionJobStatus']
+    
+    # check the status every 5 seconds and 
+    # return the transcribed text if the job is finished
+    # otherwise return None if job failed
+    while True:
+        if status == 'COMPLETED':
+            print(f"Job {job_name} completed")
+            with urllib.request.urlopen(job['TranscriptionJob']['Transcript']['TranscriptFileUri']) as r:
+                data = json.loads(r.read())
+            return data['results']['transcripts'][0]['transcript']
+        elif status == 'FAILED':
+            print(f"Job {job_name} failed")
+            return None
+        else:
+            print(f"Status of job {job_name}: {status}")
+            time.sleep(5)
+    
+# launch the job
+job_status = start_transcribe_job(transcribe, JOB_NAME, BUCKET_NAME, FILE_NAME)
+
+print(job_status)
+# if job launched successfully `job_status` will be True
+if job_status: # and we can start requesting the results from the service
+    text = get_transcription_text(transcribe, JOB_NAME)
+    print(f'The transcribed text for {FILE_NAME} file:')
+    print(text)
+else: # or print the error code if somethign went wrong
+    print(f'Job {JOB_NAME} failed with the error: {job_status}')
